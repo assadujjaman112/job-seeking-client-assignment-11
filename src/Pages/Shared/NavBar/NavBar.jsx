1e07623e0dc925d7ad6c5048be47e00b6e56ca09
assadujjaman112 <assadujjamanmurad120@gmail.com>
2023-11-08 03:41:09 +0600

    created the project

---
+++ src/Pages/Shared/NavBar/NavBar.jsx
@@ -0,0 +1,63 @@
+import { Link, NavLink } from "react-router-dom";
+import logo from "../../../assets/images/logo.png";
+
+const NavBar = () => {
+  const navLinks = (
+    <>
+      <li>
+        <NavLink
+          to="/"
+          className={({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? "bg-[#331D2C] text-white" : ""
+          }
+        >
+          Home
+        </NavLink>
+      </li>
+    </>
+  );
+  return (
+    <div className="navbar bg-base-100 md:w-4/5 mx-auto">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {navLinks}
+          </ul>
+        </div>
+        <Link to="/" className=" normal-case text-xl">
+          <div className="flex items-center">
+            <img src={logo} className="w-10 md:w-16" alt="" />
+            <h1 className="font-extrabold">iApplyNow</h1>
+          </div>
+        </Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{navLinks}</ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn">Button</a>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
