3e46a5cbe828a522e85b87f8cb6d4fbda1a626d6
assadujjaman112 <assadujjamanmurad120@gmail.com>
2023-11-12 16:43:35 +0600

    completed the all jobs serch functionality

--- src/Pages/AllJobs/AllJobs.jsx
+++ src/Pages/AllJobs/AllJobs.jsx
@@ -1,11 +1,47 @@
 import moment from "moment";
+import { useState } from "react";
 import { Link, useLoaderData } from "react-router-dom";
 
 const AllJobs = () => {
   const allJobs = useLoaderData();
+  const [displayAllJobs, setDisplayAllJobs] = useState(allJobs);
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+
+    const form = event.target;
+
+    const filteredAllJobs = allJobs?.filter((job) =>
+      job.title.toLowerCase().includes(form.search.value.toLowerCase())
+    );
+    setDisplayAllJobs(filteredAllJobs);
+  };
   return (
     <div>
       <div className="overflow-x-auto py-5 md:py-10 lg:py-16 bg-base-200  mb-5 md:mb-10 lg:mb-16">
+        <h1 className="text-center text-3xl md:text-4xl lg:text-5xl font-bold mb-5">
+          All Jobs
+        </h1>
+
+        <form onSubmit={handleSearch} className="flex justify-center mb-5">
+          <div className="form-control">
+            <input
+              type="text"
+              name="search"
+              placeholder="Search Here"
+              className="py-2 pl-5 pr-10 rounded-l-lg"
+              required
+            />
+          </div>
+          <div>
+            <input
+              className="py-2 px-6 rounded-r-lg bg-[#331D2C] text-white hover:text-black"
+              type="submit"
+              value="Search"
+            />
+          </div>
+        </form>
+
         <table className="table md:w-4/5 mx-auto">
           {/* head */}
           <thead>
@@ -14,14 +50,16 @@ const AllJobs = () => {
               <th className="font-bold text-base text-black">Job Poster </th>
               <th className="font-bold text-base text-black">Title </th>
               <th className="font-bold text-base text-black">Job Posted</th>
-              <th className="font-bold text-base text-black">Application Deadline </th>
+              <th className="font-bold text-base text-black">
+                Application Deadline{" "}
+              </th>
               <th className="font-bold text-base text-black">Salary Range</th>
               <th className="font-bold text-base text-black">Action</th>
             </tr>
           </thead>
           <tbody>
             {/* row 1 */}
-            {allJobs?.map((job, index) => (
+            {displayAllJobs?.map((job, index) => (
               <tr className="border-y-black border-x-black" key={job._id}>
                 <th>{index + 1}</th>
                 <td>{job.poster}</td>
--- src/Pages/AppliedJobs.jsx
+++ src/Pages/AppliedJobs.jsx
@@ -11,61 +11,69 @@ const AppliedJobs = () => {
   );
   const [appliedJobs, setAppliedJobs] = useState(filteredJobs);
 
-
-  const handleFilteredJob = filter => {
-    if(filter === "All"){
-        setAppliedJobs(filteredJobs);
+  const handleFilteredJob = (filter) => {
+    if (filter === "All") {
+      setAppliedJobs(filteredJobs);
+    } else if (filter === "On Site") {
+      const onSiteJobs = filteredJobs.filter(
+        (job) => job.category === "On Site"
+      );
+      setAppliedJobs(onSiteJobs);
+    } else if (filter === "Remote") {
+      const remoteJobs = filteredJobs.filter(
+        (job) => job.category === "Remote"
+      );
+      setAppliedJobs(remoteJobs);
+    } else if (filter === "Hybrid") {
+      const hybridJobs = filteredJobs.filter(
+        (job) => job.category === "Hybrid"
+      );
+      setAppliedJobs(hybridJobs);
+    } else if (filter === "Part Time") {
+      const partTimeJobs = filteredJobs.filter(
+        (job) => job.category === "Part Time"
+      );
+      setAppliedJobs(partTimeJobs);
     }
-    else if(filter === "On Site"){
-        const onSiteJobs = filteredJobs.filter(job => job.category === "On Site");
-        setAppliedJobs(onSiteJobs);
-    }
-    else if ( filter === "Remote"){
-        const remoteJobs = filteredJobs.filter(job => job.category === "Remote");
-        setAppliedJobs(remoteJobs);
-    }
-    else if(filter === "Hybrid"){
-        const hybridJobs = filteredJobs.filter(job => job.category === "Hybrid");
-        setAppliedJobs(hybridJobs);
-    }
-    else if(filter === "Part Time"){
-        const partTimeJobs = filteredJobs.filter(job => job.category === "Part Time");
-        setAppliedJobs(partTimeJobs);
-    }
-  }
+  };
   return (
     <div className="w-full bg-slate-100 py-5 md:py-10 lg:py-16 mb-5 md:mb-10 lg:mb-16">
       <h1 className="text-center text-3xl md:text-4xl lg:text-5xl font-bold">
         Applied Jobs
       </h1>
 
-      <div className="w-4/5 mx-auto">
-        <div className="dropdown">
-          <label tabIndex={0} className="btn m-1 bg-[#331D2C] text-white hover:text-black">
-            Click To Filter Job
-          </label>
-          <ul
-            tabIndex={0}
-            className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li onClick={()=> handleFilteredJob("All")}>
-              <a>All Job</a>
-            </li>
-            <li onClick={()=> handleFilteredJob("Remote")}>
-              <a>Remote Job</a>
-            </li>
-            <li onClick={()=> handleFilteredJob("On Site")}>
-              <a>On site</a>
-            </li>
-            <li onClick={()=> handleFilteredJob("Hybrid")}>
-              <a>Hybrid</a>
-            </li>
-            <li onClick={()=>handleFilteredJob("Part Time")}>
-              <a>Part Time</a>
-            </li>
-          </ul>
+      {appliedJobs?.length > 0 && (
+        <div className="w-4/5 mx-auto">
+          <div className="dropdown">
+            <label
+              tabIndex={0}
+              className="btn m-1 bg-[#331D2C] text-white hover:text-black"
+            >
+              Click To Filter Job
+            </label>
+            <ul
+              tabIndex={0}
+              className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li onClick={() => handleFilteredJob("All")}>
+                <a>All Job</a>
+              </li>
+              <li onClick={() => handleFilteredJob("Remote")}>
+                <a>Remote Job</a>
+              </li>
+              <li onClick={() => handleFilteredJob("On Site")}>
+                <a>On site</a>
+              </li>
+              <li onClick={() => handleFilteredJob("Hybrid")}>
+                <a>Hybrid</a>
+              </li>
+              <li onClick={() => handleFilteredJob("Part Time")}>
+                <a>Part Time</a>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
+      )}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 w-4/5 mx-auto my-5 md:my-10 lg:my-16">
         {appliedJobs.map((job) => (
           <AppliedJobCart key={job._id} job={job}></AppliedJobCart>
