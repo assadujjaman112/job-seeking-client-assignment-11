932ea89e7801995415a88359b835f69f395dbbb9
assadujjaman112 <assadujjamanmurad120@gmail.com>
2026-04-22 20:55:43 +0600

    added a loading spinner while the private route is loading

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -3,14 +3,14 @@ import { useContext } from 'react';
 import { AuthContext } from '../providers/AuthProvider';
 import { Navigate, useLocation } from 'react-router-dom';
 import swal from 'sweetalert';
+import PageLoader from '../components/PageLoader';
 
 const PrivateRoute = ({children}) => {
     const {user, loading} = useContext(AuthContext);
     const location = useLocation();
-    console.log(loading);
 
     if(loading){
-        return <span className="loading loading-spinner loading-lg"></span>
+        return <PageLoader />;
     }
 
     if(user){
